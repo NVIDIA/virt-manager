2acc0a480515215279fce32730cce7f3df7090ba
Ian May <ianm@nvidia.com>
2025-12-01 12:26:12 -0800

    NVIDIA: SAUCE: virtinst: add shared-mec support for CCA guests
    Add support for the shared-mec attribute in CCA launch security
    configuration. This allows users to control whether CCA Realms use
    shared or private Memory Encryption Contexts (MECs).
    
    Usage:
      --launchSecurity type=cca,sharedMec=on   # Use shared MEC
      --launchSecurity type=cca,sharedMec=off  # Use private MEC
      --launchSecurity type=cca                # QEMU default (shared)
    
    The sharedMec attribute maps to the libvirt XML shared-mec attribute
    and the QEMU rme-guest shared-mec property.
    
    Signed-off-by: Ian May <ianm@nvidia.com>
    (cherry-picked from d3182ae27df4f49185a0a94364614b40b5e22f07 https://github.com/NathanChenNVIDIA/virt-manager/commits/nvidia_unstable-5.1/)
    Signed-off-by: Nathan Chen <nathanc@nvidia.com>

--- virtinst/cli.py
+++ virtinst/cli.py
@@ -5332,6 +5332,7 @@ class ParserLaunchSecurity(VirtCLIParser):
         cls.add_arg("measurementAlgo", "measurementAlgo")
         cls.add_arg("personalizationValue", "personalizationValue")
         cls.add_arg("measurementLog", "measurementLog", is_onoff=True)
+        cls.add_arg("sharedMec", "sharedMec", is_onoff=True)
 
 
 ###########################
